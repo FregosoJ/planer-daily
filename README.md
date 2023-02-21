f82bc14be7a47c2ae8275453128d933894469cac
Jonathan Fregoso <jonathanfregoso@Jonathans-MBP.lan>
2023-02-21 10:08:47 -0800

    changing the html

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# planer-daily
+# My Daily Planner
+![licenseBadge](https://img.shields.io/badge/License-MIT-green)
+## Description 
+This app is to plan and track your daily work.
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Test](#test)
+* [Questions](#questions)
+## Installation 
+To install necessary dependencies, run the following command: 
+```
+You need to open by right clicking on the vs code page and opening in default browser.
+```
+## Usage 
+You can track your daily work and make notes through out the day.
+## License
+      
+This project is licensed under the MIT license.
+## Contributors
+N/A
+## Test
+To run tests, run the following command: 
+```
+N/A
+```
+## Questions
+If you have any questions about this project, contact me directly at Jonathanjair@yahoo.com. You can find more of my work at [FregosoJ](https://github.com/FregosoJ/)
---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+//MY PSEUDOCODE
+
+// this example is best to do static element vs dynamic
+// time/date
+// save user entry to local storage
+// if current time, block is red, if future time, green, if past time, gray (similar to timer from quiz, checking every second)
+// go to html and finish off other timeblocks √
+// as this runs, loop through timeblocks, check (id, id should be military, set id as 12, 1, 3, set a class)
+// queryselectorall(class of timeblock), if id is less, its past, if its === its current, if its > its future
+// add event listener to save button √
+// put date/time on page √
+// start interval √
+// checking time for adjusting timeblocks color
+// check local storage to see if we need to save anything on timeblocks
+// attach eventListeners to all buttons
+// best practice - keep variables where you need them as opposed to all being globally scoped
+
+// jQuery
+var timeBlockEl = $(".time-block");
+var today = dayjs().format("MMM DD, YYYY [at] hh:mm:ss a");
+var currentHour = dayjs().hour();
+var hourTimeNumber = [];
+
+for (var i = 0; i < timeBlockEl.length; i++) {
+  var elementID = timeBlockEl[i].getAttribute("id");
+  if (elementID.length === 6) hourTimeNumber.push(parseInt(elementID[5]));
+  if (elementID.length === 7)
+    hourTimeNumber.push(parseInt(elementID[5] + elementID[6]));
+}
+
+
+// FUNCTION TO UPDATE COLOR BLOCK--SHOULD THIS BE RUNNING WITH TIMER/CLOCK? -- HOW DO I KNOW IF THIS IS WORKING?
+
+function checkTime() {
+  for (var i = 0; i < hourTimeNumber.length; i++)
+    if (hourTimeNumber[i] < currentHour) {
+      timeBlockEl[i].classList.add("past");
+    } else if (hourTimeNumber[i] == currentHour) {
+      timeBlockEl[i].classList.add("present");
+    } else if (hourTimeNumber[i] > currentHour) {
+      timeBlockEl[i].classList.add("future");
+    }
+}
+
+// FUNCTION TO GET FROM LOCAL STORAGE AND DISPLAY USER INPUT ON TEXTAREA
+
+function renderUserInput() {
+
+  for (var i = 0; i < timeBlockEl.length; i++) {
+    var key = timeBlockEl[i].getAttribute("id");
+    var value = localStorage.getItem(key);
+    timeBlockEl[i].children[1].innerText = value;
+  }
+}
+
+// FUNCTION TO SAVE USER INPUT ON TEXTAREA
+
+function saveUserInput() {
+  var value = $(this).siblings(".description").val();
+  var key = $(this).parent().attr("id");
+  localStorage.setItem(key, value);
+}
+
+// TIME INTERVAL FUNCTION
+
+
+setInterval(function () {
+  var today = dayjs().format("MMM DD, YYYY [at] hh:mm:ss a");
+  $("#currentDay").text(today);
+}, 1000);
+
+$(".saveBtn").on("click", saveUserInput);
+
+checkTime();
+renderUserInput();
